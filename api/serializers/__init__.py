ee1f8b62844450f07f876515b0ce324bbd194cda
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-04-16 20:50:02 +0600

    добавление всех сереализаторов и представлений в иниты

---
+++ api/serializers/__init__.py
@@ -0,0 +1,4 @@
+from .task_serializers import TaskSerializer, TaskCreateSerializer
+from .project_serializers import ProjectSerializer, ProjectDetailSerializer
+from .status_serializers import StatusSerializer
+from .type_serializers import TypeSerializer
---
+++ api/views/__init__.py
@@ -0,0 +1,4 @@
+from .task_api_view import TaskAPIView
+from .project_api_view import ProjectAPIView
+from .status_api_view import StatusAPIView
+from .type_api_view import TypeAPIView
