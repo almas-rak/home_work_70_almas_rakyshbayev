aae7fd6922869d8fdd39c4945ee29532ef59ac20
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-04-16 20:40:10 +0600

    изменено представления
    TemplateView заменён на дженерики

--- issue_tracker/views/tasks.py
+++ issue_tracker/views/tasks.py
@@ -1,6 +1,7 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import TemplateView, ListView, CreateView, DetailView, DeleteView
 from django.db.models import Q
 from django.utils.http import urlencode
 
@@ -48,29 +49,17 @@ class IndexView(ListView):
         return context
 
 
-class CreateTask(TemplateView):
+class CreateTask(CreateView):
     template_name = 'add_task.html'
+    form_class = TaskForm
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = TaskForm()
-        return context
-
-    def post(self, request: WSGIRequest, *args, **kwargs):
-        form = TaskForm(data=request.POST)
-        if form.is_valid():
-            task = form.save()
-            return redirect('index')
-        return render(request, 'add_task.html', context={'form': form})
+    def get_success_url(self):
+        return reverse('task_detail', kwargs={'pk': self.object.pk})
 
 
-class TaskDetail(TemplateView):
+class TaskDetail(DetailView):
     template_name = 'task_detail.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
-        return context
+    model = Task
 
 
 class TaskUpdate(TemplateView):
@@ -91,16 +80,9 @@ class TaskUpdate(TemplateView):
         return render(request, 'task_update.html', context={'form': form, 'task': task})
 
 
-class DeleteTask(TemplateView):
+class DeleteTask(DeleteView):
     template_name = 'task_detail.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
-        context['delete'] = 'delete'
-        return context
-
-    def post(self, request: WSGIRequest, *args, **kwargs):
-        task = get_object_or_404(Task, pk=kwargs['pk'])
-        task.delete()
-        return redirect('index')
+    model = Task
+    context_object_name = 'task'
+    extra_context = {'delete': 'delete'}
+    success_url = reverse_lazy('index')
